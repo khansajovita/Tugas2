0b9efaaa57b3505c69c0dd72cbdcab7fefb38c60
khansajovita <khansajovita@gmail.com>
2022-09-21 21:55:33 +0700

    Revisi, tambah bonus, ubah json, dll.

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class MywatchlistItem(models.Model):
-    watched = models.CharField(max_length=50)
+    watched = models.BooleanField(default=False)
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
     release_date = models.CharField(max_length=100)
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,20 +1,31 @@
+from turtle import done
 from django.shortcuts import render
 from mywatchlist.models import MywatchlistItem
 from django.http import HttpResponse
 from django.core import serializers
 
 # Create your views here.
-def show_mywatchlist(request):
+def show_html(request):
+    listfilm = MywatchlistItem.objects.all()
+    done = 0
+    undone = 0
+    for count in listfilm:
+        if count.watched:
+            done += 1
+        else:
+            undone += 1
+
     context = {
-        'listfilm': MywatchlistItem.objects.all(),
-        'nama': 'Khansa Jovita', 'npm': '2106651686'
+        'listfilm': listfilm,
+        'nama': 'Khansa Jovita', 'npm': '2106651686',
+        'done': done, 'undone': undone
     }
     return render(request, "mywatchlist.html", context)
 
 def show_xml(request):
-    data = MywatchlistItem.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    listfilm = MywatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", listfilm), content_type="application/xml")
 
 def show_json(request):
-    data = MywatchlistItem.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    listfilm = MywatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", listfilm), content_type="application/json")
