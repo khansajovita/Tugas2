29c096bcc6248adced865b2aa27a4543759d18a8
khansajovita <khansajovita@gmail.com>
2022-09-22 02:57:12 +0700

    Update README.md dan Unit Test Tugas 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestMywatchlist(TestCase):
+    def testing_html(self):
+        response = Client().get('/mywatchlist/html')
+        self.assertEqual(response.status_code, 200)
+
+    def testing_xml(self):
+        response = Client().get('/mywatchlist/xml')
+        self.assertEqual(response.status_code, 200)
+        
+    def testing_json(self):
+        response = Client().get('/mywatchlist/json')
+        self.assertEqual(response.status_code, 200)
+
